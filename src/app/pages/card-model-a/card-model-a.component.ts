fb8f3e7c7074742edc60550750f637d873442a07
Jhosevic Castañeda <eien@wark.io>
2019-07-07 12:56:01 +0200

    fix style

--- src/app/pages/card-model-a/card-model-a.component.ts
+++ src/app/pages/card-model-a/card-model-a.component.ts
@@ -25,10 +25,6 @@ export class CardModelAComponent implements OnInit {
     this.leftBorder = `20px solid ${this.borderColor}`;
   }
 
-  onCardClick() {
-
-  }
-
   copyToClipboard() {
     // tslint:disable-next-line:max-line-length
     const template = `<center><div class="tabNpc"> <div class="contentNpc" style="background-color: ${this.cardColor}!important;border-left:20px solid ${this.borderColor}!important"> <img src="${this.headerImage}" alt=""> <div class="titleNpc"> ${this.npcName}</div><div class="guideTab"> ${this.npcDescription} </div></div><div class="origen" style="width:580px;margin-left: 128px;background: #72598e;"><a href="https://www.facebook.com/daquinoblanco">Aquino</a> &copy; Para uso exlusivo en <a href="http://demonic-city.foroactivo.com">Demonic City</a></div></center>`;
