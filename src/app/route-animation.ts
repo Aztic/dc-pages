6558b16033e87f6e500a5da70b30c404877e9295
Jhosevic Castañeda <eien@wark.io>
2019-07-08 11:21:46 +0200

    animations again

--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -15,12 +15,12 @@ export const slideInAnimation =
       query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
       group([
         query(':enter', [
-          style({ transform: 'translateX(-100%)' }),
+          style({ transform: 'translateX(100%)' }),
           animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
         ], { optional: true }),
         query(':leave', [
           style({ transform: 'translateX(0%)' }),
-          animate('0.5s ease-in-out', style({ transform: 'translateX(100%)' }))
+          animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
         ], { optional: true }),
       ])
     ]),
@@ -49,5 +49,5 @@ export const slideInAnimation =
           animate('0.5s ease-in-out', style({ transform: 'translateX(100%)' }))
         ], { optional: true }),
       ])
-    ]),
+    ])
   ]);
